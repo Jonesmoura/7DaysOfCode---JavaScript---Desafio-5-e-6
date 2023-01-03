0844879603222506e8afde2246c392d7ab932e11
Jones Moura <jones-ms@live.com>
2023-01-03 19:27:09 -0300

    correção identação

--- script.js
+++ script.js
@@ -23,96 +23,96 @@ var grupoExclusao = document.querySelector('#grupoExclusao')
 
 //function para criar ul com o array grupos
 
-function imprimirLista(grupoLista,GrupoListaString){
+function imprimirLista(grupoLista, GrupoListaString) {
 
     let lista = document.createElement('ul')
     lista.innerHTML = GrupoListaString.toUpperCase()
     lista.classList.add('TituloLista')
 
-    for(i=0;i<grupoLista.length;i++){
-        
+    for (i = 0; i < grupoLista.length; i++) {
+
         let itemLista = document.createElement('li')
         itemLista.innerHTML = grupoLista[i]
-        itemLista.classList.add('itemLista')        
+        itemLista.classList.add('itemLista')
         lista.appendChild(itemLista)
     }
 
-    if(grupoLista.length>0){
+    if (grupoLista.length > 0) {
         resposta.appendChild(lista)
     }
 
 }
 
-function mostrarlista(){
+function mostrarlista() {
 
-    if(AddlistaSimouNao.value == 'nao'){
+    if (AddlistaSimouNao.value == 'nao') {
 
         resposta.innerHTML = ''
         dadosEntrada.classList.add('toHide')
-        imprimirLista(frutas,'frutas')
-        imprimirLista(verduras,'verduras')
-        imprimirLista(doces,'doces')
-        imprimirLista(itensBasicos,'itensBasicos')
+        imprimirLista(frutas, 'frutas')
+        imprimirLista(verduras, 'verduras')
+        imprimirLista(doces, 'doces')
+        imprimirLista(itensBasicos, 'itensBasicos')
         resposta.classList.remove('toHide')
         perguntaExclusao.classList.remove('toHide')
-        
-    }else{
+
+    } else {
 
         dadosEntrada.classList.remove('toHide')
         resposta.classList.add('toHide')
         perguntaExclusao.classList.add('toHide')
         dadosExclusao.classList.add('toHide')
-        
+
     }
 }
 
-function addItem(){
-    if(grupo.value == 'frutas'){
+function addItem() {
+    if (grupo.value == 'frutas') {
         frutas.push(nomeItem.value)
-    }else if(grupo.value == 'verduras'){
+    } else if (grupo.value == 'verduras') {
         verduras.push(nomeItem.value)
-    }else if(grupo.value == 'doces'){
+    } else if (grupo.value == 'doces') {
         doces.push(nomeItem.value)
-    }else{
+    } else {
         itensBasicos.push(nomeItem.value)
     }
 
 }
 
-function mostrarFormExclusao(){
+function mostrarFormExclusao() {
 
-    if(listaExcluirSouN.value == 'sim'){
+    if (listaExcluirSouN.value == 'sim') {
 
-    dadosExclusao.classList.remove('toHide')
+        dadosExclusao.classList.remove('toHide')
 
     }
 }
 
-function definirGrupo(){
+function definirGrupo() {
 
-    if(grupoExclusao.value == 'frutas'){
-        removerItem(frutas,itemExclusao)
-    }else if(grupoExclusao.value == 'verduras'){
-        removerItem(verduras,itemExclusao)
-    }else if(grupoExclusao.value == 'doces'){
-        removerItem(doces,itemExclusao)
-    }else{
-        removerItem(itensBasicos,itemExclusao)
+    if (grupoExclusao.value == 'frutas') {
+        removerItem(frutas, itemExclusao)
+    } else if (grupoExclusao.value == 'verduras') {
+        removerItem(verduras, itemExclusao)
+    } else if (grupoExclusao.value == 'doces') {
+        removerItem(doces, itemExclusao)
+    } else {
+        removerItem(itensBasicos, itemExclusao)
     }
 
 }
 
-function removerItem(grupoExc, item){
+function removerItem(grupoExc, item) {
 
-    if(grupoExc.indexOf(item.value) == -1){
+    if (grupoExc.indexOf(item.value) == -1) {
 
         alert('Item não encontrado!')
-        
-    }else{
 
-        grupoExc.splice(grupoExc.indexOf(item.value),1)
+    } else {
+
+        grupoExc.splice(grupoExc.indexOf(item.value), 1)
         alert(`o item ${item.value} foi excluido da lista, responda novamente a primeira pergunta para atualizar a lista.`)
 
-    }   
+    }
 
 }
